dfdf21174d1afc54102126a2aaa5a3cca8faf582
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-03-25 18:31:04 +0800

    tegav2.mk: add android.hardware.touchscreen.multitouch.distinct.xml

--- tegav2.mk
+++ tegav2.mk
@@ -2,8 +2,9 @@ PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
 
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     $(LOCAL_PATH)/excluded-input-devices.xml:system/etc/excluded-input-devices.xml \
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/btn_switch:system/xbin/btn_switch
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
