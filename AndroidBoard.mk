97e1a08b4a1f51da08622d988fbc4da4b57e3181
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-05-23 14:51:55 +0800

    update for gingerbread-x86

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_FIRMWARES := ath3k-1.fw
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/tegav2_info
-TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/tegav2_defconfig
 
 include $(GENERIC_X86_ANDROID_MK)
--- tegav2.mk
+++ tegav2.mk
@@ -2,7 +2,7 @@ PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
 
 PRODUCT_COPY_FILES := \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     $(LOCAL_PATH)/excluded-input-devices.xml:system/etc/excluded-input-devices.xml \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/btn_switch:system/xbin/btn_switch
