108eac4a4d8916c81e3cc700227c9b1d07b91610
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-12-09 16:34:57 +0800

    tegav2.mk: remove unnecessary PRODUCT_COPY_FILES

--- tegav2.mk
+++ tegav2.mk
@@ -3,9 +3,6 @@ PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
 PRODUCT_PACKAGES += io_switch
 
 PRODUCT_COPY_FILES := \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    device/common/generic_x86/GenericTouch.idc:system/usr/idc/Vendor_20b3_Product_0a18.idc \
-    device/common/generic_x86/GenericTouch.idc:system/usr/idc/Vendor_03eb_Product_201c.idc \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
