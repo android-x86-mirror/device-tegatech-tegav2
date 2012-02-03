18bd2e99250dbb1402236a58919ec9ebd6809e4d
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-03 17:20:16 +0800

    handle buttons by the keylayout file
    Just use the keylayout file to map buttons to desired key and ignore
    roration events. This simplifies the design of kbdsensor.

--- tegav2.mk
+++ tegav2.mk
@@ -4,6 +4,7 @@ PRODUCT_PACKAGES += io_switch
 
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/AT_Translated_Set_2_keyboard.kl:system/usr/keylayout/AT_Translated_Set_2_keyboard.kl \
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
